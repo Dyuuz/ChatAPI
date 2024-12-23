ee519c5d89fb2d997eadef7ef6fd5cad09d05e54
Lekan Oyesunle <lekanoyesunle@gmail.com>
2024-12-23 21:29:48 +0100

    Update views.py

--- Api/views.py
+++ Api/views.py
@@ -28,16 +28,16 @@ class UserRegistration(APIView):
                 # calls the create function in serializer script to create user using the data provided in Json
                 serializer.save()
                 
-                # 
+                # Return feedback for successful registration 
                 return Response(
                     {"message" : f"{serializer.data['username']} registered successfully!"}, 
                     status=status.HTTP_200_OK)
                 
-            #   
+            # Feedback for invalid input  
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        # 
+        # Handles error exception 
         except Exception as e:
             return Response(
                 {"error": f"An unexpected error occurred: {str(e)}"},
@@ -48,29 +48,28 @@ class UserRegistration(APIView):
 class UserLogin(APIView):
     def post(self, request):
         try:
-            # 
+            # pass icnoming data to variable data from client side
             data = request.data
-            #print(f”Data: {data}")
             
-            # 
+            # Feedback if no data is inputted from client side
             if not data:
                 return Response({"username" : "This field is required", "password" : "This field is required"},
                     status=status.HTTP_401_UNAUTHORIZED)
               
-            #   
+            # Get user details from client side if data exists
             username = data.get('username')
             password = data.get('password')
             
-            # 
+            # Feedback if one of the inputs is invalid 
             if not username or not password:
                 return Response({"error" : "Username or password field cannot be empty"}, 
                     status=status.HTTP_401_UNAUTHORIZED)
                 
-            # 
+            # Ratify user input to keep the user logged in
             user = authenticate(request, username=username, password=password)
 
             try:
-                # 
+                # Proceeds with if after user is successfully authenticated
                 if user.is_authenticated:
                         login(request, user)
                         auth_token = str(uuid.uuid4())
@@ -78,7 +77,7 @@ class UserLogin(APIView):
                         user_instance = User.objects.get(username=username)
                         token_instance = Token.objects.filter(user__username=user_instance.username)\
                         
-                        # 
+                        # Rotate user token if the logged in user already has a login token
                         if token_instance.exists():
                             token = Token.objects.get(user__username=user.username)
                             print(token.token)
@@ -87,18 +86,16 @@ class UserLogin(APIView):
                             print(token.user.username)
                             return Response({"Token": auth_token}, status=status.HTTP_200_OK)
                         
-                        # 
+                        # Create a new token for user if they are just logging in for the first time
                         Token.objects.create(token=auth_token, user=user)
                         
-                        # 
+                        # Feedback after token creation
                         return Response({"Token": auth_token}, status=status.HTTP_200_OK)
-            
-            #    
+                    
             except:
                 return Response({"error" : "Invalid login details"},
                                 status=status.HTTP_400_BAD_REQUEST)
         
-        # 
         except Exception as e:
             return Response(
                {"error": f"An unexpected error occurred: {str(e)}"},
@@ -109,30 +106,30 @@ class UserLogin(APIView):
 class ChatAPI(APIView):
     def post(self, request):
         try:
-            # 
+            # pass icnoming data to variable data from client side
             data = request.data
             if not data:
                 return Response({"token" : "This field is required",
                                 "message" : "This field is required"},
                     status=status.HTTP_400_BAD_REQUEST)
               
-            #   
+            # Get input details from client side if data exists
             token = data.get('token')
             message = data['message']
             
-            # 
+            # Feedback if one of the input is invalid
             if not token or not message:
                 return Response({"error" : "Token and message are required"}, 
                     status=status.HTTP_400_BAD_REQUEST)
             
-            # 
+            # Verify if token exists from DN
             token = Token.objects.get(token=token)
             
-            # 
+            # Get user instance 
             user = request.user
             if user:
                 
-                # 
+                # Proceeds with if if user exists
                 if token:
                     response = get_ai_response(message)
                     user.tokens -= 100
