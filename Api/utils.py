e265b2de9f6d36707837778f3a17938ca4bb39f6
Dyuuz <lekanoyesunle.com>
2024-12-24 15:38:57 +0100

    push

--- Api/utils.py
+++ Api/utils.py
@@ -1,30 +1,24 @@
-import time
 from openai import OpenAI
-import os
-#from django.conf import settings
+from django.conf import settings
 
 def get_ai_response(message):
     try:
-        client = OpenAI(api_key=settings.API_KEY)
+        client = OpenAI(api_key="settings.API_KEY")
         #openai.api_key = settings.API_KEY
 
         completion = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4o-mini",
             messages=[
-                 {"role": "system", "content": "You are a helpful assistant."},
-                 {
-                   "role": "user",
-                   "content": message,
-                 }
+                {"role": "system", "content": "You are a helpful assistant."},
+                {
+                "role": "user",
+                "content": message,
+                }
             ],
+            max_tokens=120,
         )
         return completion.choices[0].message.content
         #return completion['choices'][0]['message']['content']
     
     except Exception as e:
         return f"Error: {str(e)}"
-
-if __name__ == "main":
-     user_message = "prompt"
-     ai_response = get_ai_response(user_message)
-     print(f"AI Response: {ai_response}")
