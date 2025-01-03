f8b40d9068cf279d70f4a3970d9b2e1a89a9cc30
Lekan Oyesunle <lekanoyesunle@gmail.com>
2025-01-03 12:47:05 +0100

    Added exception object to Classview Function of Chat

--- Api/views.py
+++ Api/views.py
@@ -171,7 +171,7 @@ class ChatAPI(APIView):
                             
             except Exception as e:
                 return Response(
-                    {"error": f"pls login before making a chat request"},
+                    {"error": f"pls login before making a chat request({e})"},
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
         
